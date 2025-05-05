65823133e793dda9cccf1f4e52bd2a81050c3500
nadav <nadavcohen700@gmail.com>
2025-05-05 10:10:31 +0300

    Encryption

--- Encryption.py
+++ Encryption.py
@@ -1,33 +1,39 @@
+# Simple RSA and AES encryption utilities
 from Crypto.PublicKey import RSA
 from Crypto.Cipher import PKCS1_OAEP, AES
 from Crypto.Random import get_random_bytes
-import base64
+
 class RSAEncryption:
-    def __init__(self, key_size=2048):
-        self.key = RSA.generate(key_size)
+    # RSA encryption for key exchange
+    def __init__(self):
+        self.key = RSA.generate(2048)
         self.public_key = self.key.publickey()
     def export_public_key(self):
+        # Export public key bytes
         return self.public_key.export_key()
-    def export_private_key(self):
-        return self.key.export_key()
     def encrypt_with_public_key(self, data, public_key_bytes):
-        recipient_key = RSA.import_key(public_key_bytes)
-        cipher_rsa = PKCS1_OAEP.new(recipient_key)
-        return cipher_rsa.encrypt(data)
+        # Encrypt data with a public key
+        pub_key = RSA.import_key(public_key_bytes)
+        cipher = PKCS1_OAEP.new(pub_key)
+        return cipher.encrypt(data)
     def decrypt_with_private_key(self, encrypted_data):
-        cipher_rsa = PKCS1_OAEP.new(self.key)
-        return cipher_rsa.decrypt(encrypted_data)
+        # Decrypt data with private key
+        cipher = PKCS1_OAEP.new(self.key)
+        return cipher.decrypt(encrypted_data)
+
 class AESEncryption:
+    # AES encryption for messages
     def __init__(self, key=None):
-        self.key = key or get_random_bytes(32)
+        self.key = key if key else get_random_bytes(32)
     def encrypt(self, data):
+        # Encrypt data with AES (GCM)
         cipher = AES.new(self.key, AES.MODE_GCM)
         ciphertext, tag = cipher.encrypt_and_digest(data)
-        return base64.b64encode(cipher.nonce + tag + ciphertext)
-    def decrypt(self, enc_data):
-        enc_data = base64.b64decode(enc_data)
-        nonce = enc_data[:16]
-        tag = enc_data[16:32]
-        ciphertext = enc_data[32:]
+        return cipher.nonce + tag + ciphertext
+    def decrypt(self, encrypted_data):
+        # Decrypt AES-encrypted data
+        nonce = encrypted_data[:16]
+        tag = encrypted_data[16:32]
+        ciphertext = encrypted_data[32:]
         cipher = AES.new(self.key, AES.MODE_GCM, nonce=nonce)
         return cipher.decrypt_and_verify(ciphertext, tag)
